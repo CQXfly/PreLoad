b74e626602dfc474961fd4cdb7b5d433129919f0
fox <qxchong@ctrip.com>
2018-12-14 10:53:04 +0800

    Initial Commit

---
+++ PreLoad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PreLoad
+//
+//  Created by fox on 2018/12/14.
+//  Copyright © 2018 fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
